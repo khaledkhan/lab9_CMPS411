ed312b4724e4b19dcd29ce7d3447859957971421
Khaled Khan <k.khan@qu.edu.qa>
2015-04-10 07:17:58 +0300

    my second commit to FirstJava.java

--- FirstJava.java
+++ FirstJava.java
@@ -4,4 +4,5 @@ import java.lang.*;
   {  
    System.out.println("This is my first Java program."); 
       StdOut.print("Type the first integer: ");
+       System.out.println("Type another integer."); 
    } }  

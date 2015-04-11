9dba5728f1b90b521bc4e6ea0c8be518e69c64f0
Khaled Khan <k.khan@qu.edu.qa>
2015-04-11 19:06:13 +0300

    My 6th commit

--- FirstJava.java
+++ FirstJava.java
@@ -5,5 +5,5 @@ import java.lang.*;
    System.out.println("This is my first Java program."); 
       StdOut.print("Type the first integer: ");
        System.out.println("Please Type another integer:");
-       System.out.println("Please Type 5th integer:"); 
+       System.out.println("Please Type 6th integer:"); 
    } }  
